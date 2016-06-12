280cdafdd614c0d14aeeda77c3fd38970f057896
hughie <530125717@qq.com>
2016-06-12 14:45:33 +0800

    Initial commit

---
+++ app/src/androidTest/java/vivisystem/com/i5cnc/strict_morse/ApplicationTest.java
@@ -0,0 +1,15 @@
+package vivisystem.com.i5cnc.strict_morse;
+
+import android.app.Application;
+import android.test.ApplicationTestCase;
+
+/**
+ * <a href="http://d.android.com/tools/testing/testing_android.html">Testing Fundamentals</a>
+ */
+public class ApplicationTest extends ApplicationTestCase<Application>
+{
+    public ApplicationTest()
+    {
+        super(Application.class);
+    }
+}
---
+++ app/src/test/java/vivisystem/com/i5cnc/strict_morse/ExampleUnitTest.java
@@ -0,0 +1,17 @@
+package vivisystem.com.i5cnc.strict_morse;
+
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+/**
+ * To work on unit tests, switch the Test Artifact in the Build Variants view.
+ */
+public class ExampleUnitTest
+{
+    @Test
+    public void addition_isCorrect() throws Exception
+    {
+        assertEquals(4, 2 + 2);
+    }
+}

ac9fce8db0efc010e67566d40aecf232321335b0
hughie <530125717@qq.com>
2016-06-12 15:12:46 +0800

    First testing codes

--- app/src/main/java/vivisystem/com/i5cnc/strict_morse/MainActivity.java
+++ app/src/main/java/vivisystem/com/i5cnc/strict_morse/MainActivity.java
@@ -2,6 +2,7 @@ package vivisystem.com.i5cnc.strict_morse;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.util.Log;
 
 public class MainActivity extends AppCompatActivity
 {
@@ -11,5 +12,6 @@ public class MainActivity extends AppCompatActivity
     {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        Log.d("Hughie", "MainActivity.onCreate() called finished.");
     }
 }
